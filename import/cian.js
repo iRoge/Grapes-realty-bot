6707e6851d44de07d94aa4b90579ea280f64c39c
Egor <iroge-27@yandex.ru>
2022-02-06 22:44:46 +0300

    1

--- import/cian.js
+++ import/cian.js
@@ -22,18 +22,30 @@ async function run() {
 
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
-    page.on('console', msg => {
-        console.log(msg.text());
-    });
+    // page.on('console', msg => {
+    //     console.log(msg.text());
+    // });
 
     await page.goto('https://www.cian.ru/cat.php?deal_type=rent&engine_version=2&is_by_homeowner=1&offer_type=flat&region=1&sort=creation_date_desc&type=4');
     let data = await page.evaluate(() => {
         let announcements = document.querySelectorAll('._93444fe79c--card--ibP42');
+        let numbers = [];
         for (let an of announcements) {
-            let 
+            let button = an.querySelector('button span._93444fe79c--text--rH6sj');
+            if (!button) {
+                continue;
+            }
+            button.click();
+            let numbersDivs = an.querySelectorAll('div._93444fe79c--button--j934Y div._93444fe79c--container--aWzpE > span[data-mark="PhoneValue"]');
+            for (let numberDiv of numbersDivs) {
+                numbers.push(numberDiv.innerHTML.trim());
+            }
+            
         }
-
+        return numbers;
     });
+
+    console.log(data);
 
     await page.waitForTimeout(10000);
 }
