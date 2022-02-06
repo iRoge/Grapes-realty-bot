9510956a3002d6500b9782eb3d9990eeb05d9258
iRoge <iroge-27@yandex.ru>
2022-02-07 00:45:59 +0300

    1

--- import/cian.js
+++ import/cian.js
@@ -12,7 +12,7 @@ async function run() {
         // '--proxy-server=188.113.190.7:80'
     ];
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: args,
         defaultViewport: {
             width: 1920,
@@ -22,30 +22,54 @@ async function run() {
 
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
-    // page.on('console', msg => {
-    //     console.log(msg.text());
-    // });
+    page.on('console', msg => {
+        console.log(msg.text());
+    });
 
     await page.goto('https://www.cian.ru/cat.php?deal_type=rent&engine_version=2&is_by_homeowner=1&offer_type=flat&region=1&sort=creation_date_desc&type=4');
-    let data = await page.evaluate(() => {
+    let newAnnounces = await page.evaluate(() => {
         let announcements = document.querySelectorAll('._93444fe79c--card--ibP42');
-        let numbers = [];
+        let newAnnounces = [];
         for (let an of announcements) {
             let button = an.querySelector('button span._93444fe79c--text--rH6sj');
             if (!button) {
                 continue;
             }
+
+            let url = an.querySelector('div[data-name="LinkArea"] a._93444fe79c--link--eoxce').getAttribute('href');
+            let match = url.match(/\/rent\/flat\/(\d+)\//);
+            let primaryKey = match[1];
+            /** Здесь должны быть проверки на уникальность перед записью **/
+
+            let now = new Date();
+            let newAnnounce = {
+                id: primaryKey,
+                time: now.toDateString(),
+                title: null,
+                rooms: null,
+                announcesByPhone: null,
+                views: null,
+                noRealtor: null,
+                cost: null,
+                telephones: [],
+                metro: null,
+                photo: null,
+                city: null,
+            };
             button.click();
             let numbersDivs = an.querySelectorAll('div._93444fe79c--button--j934Y div._93444fe79c--container--aWzpE > span[data-mark="PhoneValue"]');
             for (let numberDiv of numbersDivs) {
-                numbers.push(numberDiv.innerHTML.trim());
+                newAnnounce.telephones.push(numberDiv.innerHTML.trim());
             }
-            
+
+            newAnnounce.title = an.querySelector('span[data-mark="OfferTitle"] span').innerHTML;
+            newAnnounce.metro = an.querySelector('a._93444fe79c--link--BwwJO div:nth-child(2)').innerHTML;
+
+            newAnnounces.push(newAnnounce)
         }
-        return numbers;
+        return newAnnounces;
     });
 
-    console.log(data);
-
+    console.log(newAnnounces);
     await page.waitForTimeout(10000);
 }
